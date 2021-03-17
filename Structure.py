809e7f91915fcf0f27ab09eca76c45c3549ee692
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-17 13:29:04 +0000

    file parser done

--- Structure.py
+++ Structure.py
@@ -6,12 +6,17 @@ Created on Sat Mar 13 12:11:57 2021
 @author: leonorgomes
 """
 
+
 class Video:
-    def __init__(self, size):
+    def __init__(self, size, id):
         self.size = size
-    
+        self.id=id
+
+
+
 class CacheServer: 
-    def __init__(self, maxCapacity):
+    def __init__(self, maxCapacity,id):
+        self.id=id
         self.maxCapacity = maxCapacity
         self.videos = []
         self.currentCapacity = 0
@@ -24,16 +29,22 @@ class CacheServer:
             return True
         return False
 
+
 class Endpoint:
-    def __init__(self, latency):
+    def __init__(self, latency,id):
+        self.id=id
         self.latency = latency
         self.dic = {}
     
     def addCacheServer(self, cacheServer, latency):
         self.dic[cacheServer] = latency
 
+    
+
+
 class Request:
-    def __init__(self, video, endpoint):
+    def __init__(self, video, endpoint,ammount, id):
         self.video = video
         self.endpoint = endpoint
-    
+        self.ammount = ammount
+        self.id=id
